5ad6d039344aa4627572a1c97c60103ccba8179d
Quoc Tin <tin.9a1.dt@gmail.com>
2026-01-18 15:03:33 +0700

    Get user by id and remove name for user

---
+++ Migrations/20260118080234_removeNameForUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobNexus.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeNameForUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
